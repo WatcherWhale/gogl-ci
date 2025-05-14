28cb04a502101bd87bb10ef943d650acf2d86e42
Mathias Maes <mathias.maes@aloxy.io>
2025-05-14 15:54:21 +0200

    chore(nix): use gomod2nix

--- flake.nix
+++ flake.nix
@@ -1,91 +1,94 @@
 {
   description = "A cli tool for getting insight into your gitlab pipelines";
 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+      devenv,
+      gomod2nix,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        imports = [
+          flake-parts.flakeModules.easyOverlay
+          devenv.flakeModule
+        ];
+
+        systems = nixpkgs.lib.systems.flakeExposed;
+
+        perSystem =
+          {
+            config,
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            overlayAttrs = {
+              inherit (config.packages) gogl-ci;
+            };
+
+            packages.default = config.packages.gogl-ci;
+
+            packages.gogl-ci = gomod2nix.legacyPackages."${system}".buildGoApplication {
+              pname = "gogl-ci";
+              version = "0.0.0";
+
+              src = "${self}";
+              pwd = "${self}";
+              modules = "${self}/gomod2nix.toml";
+
+              meta = {
+                mainProgram = "gogl";
+              };
+
+              ldflags = [
+                "-w"
+                "-s"
+              ];
+
+              subPackages = [ "cmd/cli" ];
+
+              postInstall = ''
+                mv $out/bin/cli $out/bin/gogl
+              '';
+
+            };
+
+            devenv.shells.default = {
+              packages = with pkgs; [
+                go
+                gotools
+                golangci-lint
+                go-task
+                yaegi
+                gomod2nix.packages."${system}".default
+              ];
+            };
+          };
+      }
+    );
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
+
     devenv = {
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-parts,
-    devenv,
-    ...
-  } @ inputs: let
-    version = "0.0.2";
-    rev = "e052d8e881f30ea89fcffad150bdce60e2fdc6bd";
-    hash = "sha256-h20+8EtuYL6mwyaqgIvYAxWnUfUbISVmcsWp/LfUVtA=";
-    vendorHash = "sha256-H1pwnlwvegDZ20iag8wltaE7BJg4mPV7MloE0GLuQpM=";
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
-      imports = [
-        flake-parts.flakeModules.easyOverlay
-        devenv.flakeModule
-      ];
-
-      systems = nixpkgs.lib.systems.flakeExposed;
-
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: {
-        overlayAttrs = {
-          inherit (config.packages) gogl-ci;
-        };
-
-        formatter = pkgs.alejandra;
-
-        packages.default = config.packages.gogl-ci;
-
-        packages.gogl-ci = nixpkgs.legacyPackages.x86_64-linux.buildGoModule {
-          pname = "gogl-ci";
-          version = "${version}";
-
-          vendorHash = "${vendorHash}";
-          proxyVendor = true;
-          tags = ["netgo"];
-
-          src = nixpkgs.legacyPackages.x86_64-linux.fetchgit {
-            name = "gogl-ci";
-            url = "https://github.com/WatcherWhale/gogl-ci.git";
-            rev = "${rev}";
-            hash = "${hash}";
-          };
-
-          ldflags = [
-            "-w"
-            "-s"
-          ];
-
-          meta = {
-            descirption = "GoGL-CI: A cli tool for getting insight into your gitlab pipelines";
-            mainProgram = "gogl";
-          };
-
-          subPackages = ["cmd/cli"];
-
-          postInstall = ''
-            mv $out/bin/cli $out/bin/gogl
-          '';
-        };
-
-        devenv.shells.default = {
-          packages = with pkgs; [
-            go
-            gotools
-            golangci-lint
-            go-task
-            alejandra
-            yaegi
-          ];
-        };
-      };
-    });
 }
